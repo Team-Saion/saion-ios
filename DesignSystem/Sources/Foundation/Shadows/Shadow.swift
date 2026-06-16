81c2e67467d85a6619edf6b442c462d75266f15a
chunim98 <jjingeo1230@naver.com>
2026-06-16 14:32:44 +0900

    chore: Shadow 토큰 추가

---
+++ DesignSystem/Sources/Foundation/Shadows/Shadow.swift
@@ -0,0 +1,31 @@
+//
+//  Shadow.swift
+//  DesignSystem
+//
+//  Created by 신정욱 on 6/16/26.
+//
+
+import UIKit
+
+public struct Shadow: Sendable {
+    public let shadowColor: UIColor
+    public let shadowOpacity: CGFloat
+    public let shadowOffset: CGSize
+    public let shadowRadius: CGFloat
+}
+
+public extension Shadow {
+    static let container = Shadow(
+        shadowColor: .black,
+        shadowOpacity: 0.08,
+        shadowOffset: CGSize(width: 0, height: 4),
+        shadowRadius: 10
+    )
+
+    static let component = Shadow(
+        shadowColor: .black,
+        shadowOpacity: 0.16,
+        shadowOffset: CGSize(width: 0, height: 0),
+        shadowRadius: 24
+    )
+}
