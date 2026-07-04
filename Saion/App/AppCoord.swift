8a584de15b2c3f65a0b372da16f37df6851cf2b4
chunim <jjingeo1230@naver.com>
2026-07-04 22:18:49 +0900

    chore: ci 테스트

--- Saion/App/AppCoord.swift
+++ Saion/App/AppCoord.swift
@@ -41,6 +41,7 @@ final class AppCoord: Coordinator {
         // 새 코디네이터의 네비게이션을 루트로 설정
         let vc = HomeVC()
         setRootWithAnimation(vc)
+        
 //        let coord = TabBarCoord(navigation: .init())
 //        setRootWithAnimation(coord.navigation)
 //        store(child: coord)
